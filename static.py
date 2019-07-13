c7bd1016e0924f17722c3134a6d35add3d4aa30b
Hayk Hovsepyan <hayk.hovsepyan@betconstruct.com>
2019-07-13 22:02:29 +0400

    new change

--- static.py
+++ static.py
@@ -5,14 +5,12 @@ import argparse
 
 class Static():
     def __init__(self, comp_path):
-        self.parser = argparse.ArgumentParser(description="Coin Fork arguments descriptions.")
-        #self.parser.add_argument("a")
-        self.parser.add_argument("--btc", type=float,default=0.1,help='Btc size to buy coin, default 0.1BTC')
-        self.parser.add_argument("--perc", type=float,default=0,help='Percent to view result, default higher 0%')
+        self.parser = argparse.ArgumentParser(description="Coin Fork.")
+        self.parser.add_argument("--btc", type=float,default=0.1,help='Btc size to buy currency, (default: %(default)s)')
+        self.parser.add_argument("--perc", type=float,default=0,help='Percent difference, (default: Higher %(default)s)')
         self.args = self.parser.parse_args()
         self.size_count = 0
         self.total_count = 0
-        #self.btc_size = float(self.args.a)
         self.btc_size = self.args.btc
         self.size_count_sell = 0
         self.total_count_sell = 0
